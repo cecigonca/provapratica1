c8067218975afae8bdb84cb15e99a991f925d7d0
Cecilia <cecilia.goncalves@sou.inteli.edu.br>
2024-03-08 16:22:06 -0300

    Commit final
    Criação das rotas e documentação no README

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# provapratica1
+# Parte Prática - Prova 1
+## Cecília Gio Alonso Gonçalves
+
+## Descrição
+Esse repositório contém o deselvolvimento da parte prática da prova 1 Módulo 5. O objetivo final da questão é projeto Web implementado com o micro framework Flask em Python, que siga expecificações citadas no enunciado. 
+
+## Pré-requisitos
+**Python 3 instalado**
+**Clonar o repositório**
+
+## Guia de Instalação (Windows, através do terminal)
+Na janela do terminal digite os seguintes comandos para cada dependência
+
+**Criando Ambiente Virtual**
+
+```python -m venv venv```
+```venv\Scripts\activate```
+
+**Instalar as dependências necessárias**
+
+- flask 
+```pip install flask```
+- tinydb
+```pip install tinydb```
+- requirements.txt
+```pip install -r requirements.txt```
+```pip freeze > requirements.txt```
+
+**Para executar a aplicação**
+```python -m flask --app src\prova-web\app.py run --host 0.0.0.0 --port 8000```
+
+**Descricão dos Arquivos**
--- src/prova-web.py/app.py
+++ src/prova-web.py/app.py
@@ -1,19 +1,53 @@
-# app.py
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from tinydb import TinyDB
 
 app = Flask(__name__)
+db = TinyDB('caminhos.json')
 
-@app.route("/novo")
+@app.route("/novo", methods=['POST'])
+def novo_caminho():
+    if request.method == "POST":
+        dados = request.json
+    db.insert(dados)
+    dados = request.json
+    id = db.insert(dados)
+    return render_template()
 
-@app.route("/pegar_caminho")
+@app.route("/pegar_caminho", methods=['GET'])
+def pegar_caminho():
+    if request.method == "GET":
+        dados = request.json
+    db.insert(dados)
+    dados = request.json
+    id = db.insert(dados)
+    return render_template()
 
-@app.route("/listas_caminhos")
+@app.route("/listas_caminhos", methods=['GET'])
+def listar_caminhos():
+    if request.method == "GET":
+        dados = request.json
+    db.insert(dados)
+    dados = request.json
+    id = db.insert(dados)
+    return render_template()
 
-@app.route("/atualizar")
+@app.route("/atualizar", methods=['POST'])
+def atualizar_caminho(id):
+    if request.method == "POST":
+        dados = request.json
+    db.insert(dados)
+    dados = request.json
+    id = db.insert(dados)
+    return render_template()
 
-@app.route("/deletar")
-
-
+@app.route("/deletar", methods=['DELETE'])
+def deletar_caminho(id):
+    if request.method == "DELTE":
+        dados = request.json
+    db.insert(dados)
+    dados = request.json
+    id = db.insert(dados)
+    return render_template()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
