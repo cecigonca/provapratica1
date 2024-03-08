c618de18287680185c3480c4a68308f02264ef2e
Cecilia <cecilia.goncalves@sou.inteli.edu.br>
2024-03-08 16:03:44 -0300

    Arquivos e teste
    Adicionei os arquivos necessários para o desenvolvimento e fiz um teste do flask

---
+++ src/ola-flask.py
@@ -0,0 +1,8 @@
+# testando
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Ola Mundo!!</p>"
---
+++ src/prova-web.py/app.py
@@ -0,0 +1,19 @@
+# app.py
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/novo")
+
+@app.route("/pegar_caminho")
+
+@app.route("/listas_caminhos")
+
+@app.route("/atualizar")
+
+@app.route("/deletar")
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
